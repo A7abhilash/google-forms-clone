937692ba9ee7dd76aa57ef644244cd6dd5fa3873
A7abhilash <a7abhilash@gmail.com>
2022-03-20 15:21:11 +0530

    feat: edit-form /fe

--- src/components/create-form/CreateForm.jsx
+++ src/components/create-form/CreateForm.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForms } from "../../App";
 import FieldsContainer from "./FieldsContainer";
 import FormHeader from "./FormHeader";
 
-function CreateForm() {
+function CreateForm({ form }) {
   const { formsAppContract, address, getErrorMessage } = useForms();
 
   const [title, setTitle] = useState("");
@@ -11,6 +11,25 @@ function CreateForm() {
   const [endTime, setEndTime] = useState("");
   const [allowResponse, setAllowResponse] = useState(true);
   const [fields, setFields] = useState([]);
+
+  useEffect(() => {
+    if (form) {
+      setTitle(form.title);
+      setDescription(form.description);
+      setAllowResponse(form.allowResponse);
+      setFields(form.fields);
+
+      let date = new Date(Number(form.endTime));
+      date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+      setEndTime(date.toISOString().slice(0, 16));
+    } else {
+      setTitle("");
+      setDescription("");
+      setEndTime("");
+      setAllowResponse(true);
+      setFields([]);
+    }
+  }, [form]);
 
   const createForm = async () => {
     try {
@@ -40,14 +59,45 @@ function CreateForm() {
     }
   };
 
+  const editForm = async () => {
+    try {
+      // console.log(title);
+      // console.log(description);
+      // console.log(endTime);
+      // console.log(allowResponse);
+      // console.log(fields);
+
+      formsAppContract.methods
+        .editForm(
+          form.id,
+          title,
+          description,
+          endTime ? new Date(endTime).getTime().toString() : "",
+          allowResponse,
+          fields.length,
+          fields
+        )
+        .send({ from: address })
+        .on("receipt", () => {
+          alert("Successfully edited your form...");
+          // window.location.reload();
+        });
+    } catch (error) {
+      console.log(error);
+      // alert(getErrorMessage(error));
+    }
+  };
+
   return (
     <div className="mt-5 row">
       <div className="col-md-7 bg-light p-2 mx-2 mx-md-auto">
         <div className="row">
-          <div className="col-12 pb-2">
-            <h4 className="text-center">Create Form</h4>
-            <hr />
-          </div>
+          {!form && (
+            <div className="col-12 pb-2">
+              <h4 className="text-center">Create Form</h4>
+              <hr />
+            </div>
+          )}
           <FormHeader
             title={title}
             setTitle={setTitle}
@@ -71,9 +121,9 @@ function CreateForm() {
               </button>
               <button
                 className="btn btn-block btn-success"
-                onClick={createForm}
+                onClick={!form ? createForm : editForm}
               >
-                Create
+                {!form ? "Create" : "Edit"}
               </button>
             </div>
           </div>
--- src/components/edit-form/EditForm.jsx
+++ src/components/edit-form/EditForm.jsx
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Tab, Tabs } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useForms } from "../../App";
+import Loading from "../../containers/Loading";
+import CreateForm from "../create-form/CreateForm";
 
 function EditForm() {
   const { id } = useParams();
+  const { formsAppContract, address } = useForms();
+
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState(null);
+
+  const getForm = async () => {
+    setLoading(true);
+    try {
+      const _form = await formsAppContract.methods.forms(id).call();
+      if (_form.id === "0") {
+        alert("Invalid form...");
+        window.location.replace("/");
+      }
+
+      if (address !== _form.owner) {
+        // window.location.replace(`/view-form/${id}`);
+        alert("Arrey bhai kya kar raha hain");
+        window.location.replace(`/`);
+      }
+
+      const _fields = await formsAppContract.methods.getFields(id).call();
+      // get submissions
+      _form["fields"] = _fields;
+      console.log(_form);
+      // _form["submissions"]=_submissions
+      setForm(_form);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getForm();
+    }
+  }, [id]);
+
+  if (loading) {
+    return <Loading loadingMsg="Loading form..." />;
+  }
 
   return (
-    <div className="mt-5">
-      EditForm
-      <p>{id}</p>
+    <div className="row mt-5 bg-light p-2 rounded">
+      <div className="col-12 text-center">
+        <h3>Edit Form</h3>
+      </div>
+      <div className="col-12 mx-1 my-2">
+        <Tabs>
+          <Tab eventKey="edit" title="Edit">
+            <CreateForm form={form} />
+          </Tab>
+          <Tab eventKey="submissions" title="Submissions">
+            Submissions
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
