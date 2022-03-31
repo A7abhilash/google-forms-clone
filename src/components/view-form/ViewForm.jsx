20ed92bc6811bfc8a021bf4892ecf3529fe62a21
A7abhilash <a7abhilash@gmail.com>
2022-03-31 15:18:59 +0530

    feat: submit form /fe

--- src/components/view-form/ViewForm.jsx
+++ src/components/view-form/ViewForm.jsx
@@ -14,6 +14,7 @@ function ViewForm() {
 
   // const [answers, setAnswers] = useState([])
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [mySubmissions, setMySubmissions] = useState([]);
 
   const [isSubmittingAllowed, setIsSubmittingAllowed] = useState(true);
 
@@ -41,6 +42,14 @@ function ViewForm() {
       }
 
       //   TODO: Check if current user has submitted the form. If yes, then retrieve the submission!
+      const _submission = await formsAppContract.methods
+        .getUserSubmission(id)
+        .call({ from: address });
+      // console.log(_submission.answers);
+      if (_submission.user === address) {
+        setIsSubmitted(true);
+        setMySubmissions(_submission.answers);
+      }
     } catch (error) {
       console.log(error);
     } finally {
@@ -54,7 +63,10 @@ function ViewForm() {
     }
   }, [id]);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isSubmittingAllowed) return;
+
     try {
       let now = new Date().getTime();
       let endTime = new Date(Number(form.endTime)).getTime();
@@ -63,6 +75,45 @@ function ViewForm() {
         alert("This form is not taking the responses anymore...");
         window.location.reload();
       }
+
+      // handle submission
+
+      let _submission = [];
+
+      fields.forEach((field) => {
+        // console.log(e.target[field.title]);
+        let options = [];
+        if (field.fieldType === "checkbox") {
+          e.target[field.title].forEach((item) => {
+            if (item.checked) {
+              options.push(item.value);
+            }
+          });
+        } else {
+          if (e.target[field.title].value) {
+            options.push(e.target[field.title].value);
+          }
+        }
+        if (!options.length) {
+          options.push("");
+        }
+
+        _submission.push({
+          id: field.id,
+          title: field.title,
+          fieldType: field.fieldType,
+          options,
+        });
+      });
+      // console.log(_submission);
+
+      formsAppContract.methods
+        .submitForm(id, _submission)
+        .send({ from: address })
+        .on("receipt", () => {
+          alert("form submitted");
+          window.location.reload();
+        });
     } catch (error) {
       console.log(error);
     }
@@ -71,6 +122,7 @@ function ViewForm() {
   if (loading) {
     return <Loading loadingMsg="Loading form..." />;
   }
+
   return (
     <div className="row mt-5 bg-light p-2 rounded">
       <div className="col-md-8 mx-1 mx-md-auto my-3">
@@ -87,45 +139,65 @@ function ViewForm() {
       <hr />
       {isSubmittingAllowed ? (
         <div className="col-md-8 mx-1 mx-md-auto my-3">
-          <ListGroup>
-            {fields.map((field) => (
-              <ListGroup.Item key={field.id} variant="light">
-                <h6>{field.title}</h6>
-                {field.fieldType === "text" ? (
-                  <textarea
-                    className="form-control"
-                    placeholder="Your Answer..."
-                  ></textarea>
-                ) : (
-                  field.options.map((option, index) => (
-                    <div key={`Option+${field.id}-${index}`}>
-                      <input
-                        type={field.fieldType}
-                        name={field.title}
-                        value={option}
-                        // onChange={ }
-                      />
-                      <label
-                        htmlFor={field.title}
-                        style={{ marginLeft: 10, marginBottom: 5 }}
-                      >
-                        {option}
-                      </label>
-                      <br />
-                    </div>
-                  ))
-                )}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-          {!isSubmitted && (
-            <div>
-              <hr />
-              <button className="btn btn-primary" onClick={handleSubmit}>
-                Submit
-              </button>
-            </div>
+          {isSubmitted && (
+            <h5 className="text-center text-primary">Your Response:</h5>
           )}
+          <form onSubmit={handleSubmit}>
+            <ListGroup>
+              {fields.map((field, i) => (
+                <ListGroup.Item key={field.id} variant="light">
+                  <h6>{field.title}</h6>
+                  {field.fieldType === "text" ? (
+                    isSubmitted ? (
+                      <p> {mySubmissions[i][0]}</p>
+                    ) : (
+                      <textarea
+                        className="form-control bg-light"
+                        placeholder="Your Answer..."
+                        name={field.title}
+                      ></textarea>
+                    )
+                  ) : (
+                    field.options.map((option, index) => (
+                      <div key={`Option+${field.id}-${index}`}>
+                        {isSubmitted ? (
+                          <input
+                            type={field.fieldType}
+                            name={field.title}
+                            value={option}
+                            // disabled={isSubmitted}
+                            checked={mySubmissions[i].includes(option)}
+                            readOnly
+                          />
+                        ) : (
+                          <input
+                            type={field.fieldType}
+                            name={field.title}
+                            value={option}
+                          />
+                        )}
+                        <label
+                          htmlFor={field.title}
+                          style={{ marginLeft: 10, marginBottom: 5 }}
+                        >
+                          {option}
+                        </label>
+                        <br />
+                      </div>
+                    ))
+                  )}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+            {!isSubmitted && (
+              <div>
+                <hr />
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            )}
+          </form>
         </div>
       ) : (
         <div className="col-md-8 mx-1 mx-md-auto my-3">
